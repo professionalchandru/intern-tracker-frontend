cc48191ed18ae10275c70c3e16effa6ccd4b972b
chandru <professionalchandru@gmail.com>
2021-03-23 16:53:36 +0530

    base url updated

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,2 +1,5 @@
-const baseUrl = "https://heroku-trys.herokuapp.com" || "http://localhost:5000";
+const baseUrl =
+    "https://timeline-intern.herokuapp.com" ||
+    "https://heroku-trys.herokuapp.com" ||
+    "http://localhost:5000";
 export default baseUrl;
